1060e3d652534498a553e2da23a3496c04a1f900
Rrr <renaud.drieux@gmail.com>
2025-05-07 21:01:48 +0200

    Sauvegarde locale avant rebase

--- app.py
+++ app.py
@@ -25,3 +25,9 @@ if __name__ == '__main__':
     # Assure-toi que l'encodage est correct (utile si variable PYTHONUTF8 non définie)
     os.environ["PYTHONIOENCODING"] = "utf-8"
     app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
